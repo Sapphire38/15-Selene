abc54e1c7f7cc299134813c2434fd94d8c093e6e
Juani <juanygr@hotmail.com>
2023-01-11 18:10:55 -0300

    origins

---
+++ index.js
@@ -0,0 +1,54 @@
+    // This is an example with default parameters
+
+    simplyCountdown('.timer', {
+        year: 2023, // required
+        month: 3, // required
+        day: 25, // required
+        hours: 22, // Default is 0 [0-23] integer
+        minutes: 0, // Default is 0 [0-59] integer
+        seconds: 0, // Default is 0 [0-59] integer
+        words: { //words displayed into the countdown
+            days: { singular: '', plural: '' },
+            hours: { singular: '', plural: '' },
+            minutes: { singular: '', plural: '' },
+            seconds: { singular: '', plural: '' }
+        },
+        plural: true, //use plurals
+        inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
+        inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
+        // in case of inline set to false
+        enableUtc: false, //Use UTC or not - default : false
+        onEnd: function() { return; }, //Callback on countdown end, put your own function here
+        refresh: 1000, // default refresh every 1s
+        sectionClass: 'simply-section', //section css class
+        amountClass: 'simply-amount', // amount css class
+        wordClass: 'simply-word', // word css class
+        zeroPad: true,
+        countUp: false
+});
+
+
+
+
+
+var mensajes
+let nombre =  document.getElementById("nombre")
+let option = document.getElementById("option")
+function  enviar(){
+     
+    if(option.value == "voy"){
+        mensajes = ` <a id="envio" href="https://api.whatsapp.com/send?phone=3517710599&text=Hola, soy ${nombre.value} y voy a los 15 de Sele.">enviar</a>`
+    }else{
+        mensajes = ` <a id="envio" href="https://api.whatsapp.com/send?phone=3517710599&text=Hola, soy ${nombre.value} y no puedo ir a los 15 de Sele.">enviar</a>`
+    }
+
+   
+ 
+    document.getElementById("link").innerHTML = mensajes
+
+    document.getElementById("envio").click()
+
+
+  
+
+}
